ac7449401b4a10cee65d0a5dd7b812a4d0159d53
Paul-Hydra-Service <paul.hydraservice@gmail.com>
2024-06-19 13:39:43 -0500

    Changes made
    too many to say

--- Hydra_Service_Servo_Project_Final.ino
+++ Hydra_Service_Servo_Project_Final.ino
@@ -15,6 +15,7 @@
 *******************************************************************************/
 
 #include <Dynamixel2Arduino.h>
+#include <ServoProject.h>
 
 // Please modify it to suit your hardware.
 #if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560) // When using DynamixelShield
@@ -59,21 +60,34 @@ Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 //This namespace is required to use Control table item names
 using namespace ControlTableItem;
 
+/*
+* DO NOT CHANGE THESE
+* They are based on the hardware used and not subject to change
+*/
  // Joystick settings
 #define JOYSTICK_PIN            A3
 #define JOYSTICK_MIN            0.0
 #define JOYSTICK_MAX            1020.0
-#define JOYSTICK_DEADZONE       75.0
 #define CENTER_TAP              A0
 
 // Servo settings
 #define SERVO_MIN_ANGLE         150  //149
 #define SERVO_MAX_ANGLE         211  //225
-#define SERVO_MAX_VELOCITY      50
 
 // Testing settings
 #define FORWARDS                true
 #define BACKWARDS               false
+
+/*
+* These are subject to change
+* User may change the number to fit their prefrences.
+*/
+#define JOYSTICK_DEADZONE       75.0  // 75 is the default. Range: 0-510
+#define SERVO_MAX_VELOCITY      50    // 50 is the default. Range: 1-32767
+#define LOOP_SLOW_VELOCITY      20    // 20 is the default. Range: 0-30
+
+ServoProject servo(SERVO_MAX_ANGLE, SERVO_MIN_ANGLE, DXL_ID, 182);
+ServoProject joystick(JOYSTICK_MAX, JOYSTICK_DEADZONE, JOYSTICK_PIN, CENTER_TAP);
 
 void setup() {
   // Initialize serial communication for debugging
@@ -102,7 +116,7 @@ void setup() {
   dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, SERVO_MAX_VELOCITY);
 
   // sets the pin for the limit switch
-  pinMode(1, INPUT_PULLUP);
+  pinMode(0, INPUT_PULLUP);
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
@@ -110,296 +124,97 @@ void setup() {
 
 }
 
-// loop global variables
-bool switchPress_1 = false; // left most switch
-bool switchPress_2 = false; // second switch to the left
-bool switchPress_3 = false; // second switch to the right
-bool switchPress_4 = true; // right most switch
-int vel = 0; // velocity?
-
-// global variables for auto-calibration
-int joystickMax = JOYSTICK_MAX; // Maximum angle of the joystick
-int counter = 0; // how many times the backups is used
-
-// testing global Variables
-bool start = true; // if the testing functions have started
-unsigned long startTime; //The starting time 
-int angle = 182; // The desired angle of the servo
-bool up = true; // If moving away from angle 0
-bool pass = false; // if the second has already passed
-bool overRide = false; // gives back control to joystick
-
-// cycles the motion of the pump using the limit switches
-void cycleMotion()
-{
-  if(switchPress_4 && !switchPress_2)
-  {
-    dxl.setGoalPosition(DXL_ID, SERVO_MAX_ANGLE, UNIT_DEGREE);
-    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 80);
-    dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, 20);
-  }
-  else if (switchPress_4 && switchPress_2)
-  {
-    dxl.setGoalPosition(DXL_ID, SERVO_MAX_ANGLE, UNIT_DEGREE);
-    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 40);
-    dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, -50);
-  }
-  else if (switchPress_1 && switchPress_2)
-  {
-    dxl.setGoalPosition(DXL_ID, SERVO_MIN_ANGLE, UNIT_DEGREE);
-    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 40);
-    dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, 50);
-  }
-  else if (switchPress_1 && !switchPress_3)
-  {
-    dxl.setGoalPosition(DXL_ID, SERVO_MIN_ANGLE, UNIT_DEGREE);
-    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 80);
-    dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, 20);
-  }
-  else if (switchPress_1 && switchPress_3)
-  {
-    dxl.setGoalPosition(DXL_ID, SERVO_MIN_ANGLE, UNIT_DEGREE);
-    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 40);
-    dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, -50);
-  }
-  else if (switchPress_4 && switchPress_3)
-  {
-    dxl.setGoalPosition(DXL_ID, SERVO_MAX_ANGLE, UNIT_DEGREE);
-    dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, 40);
-    dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, 50);
-  }
-}
-
-// reads the current postion in the cycle based on the limit switches
-void readCyclePosition()
-{
-
-  // Reads the limit switches to find the current position. 
-  if(switchPress_4 && !switchPress_3 && !switchPress_2 && !switchPress_1)
-  {
-    switchPress_2 = digitalRead(2);
-  }
-  else if (switchPress_4 && !switchPress_3 && switchPress_2 && !switchPress_1)
-  {
-    switchPress_1 = digitalRead(1);
-    if(switchPress_1)
-    {
-      switchPress_4 = false;
-    }
-  }
-  else if (!switchPress_4 && !switchPress_3 && switchPress_2 && switchPress_1)
-  {
-    switchPress_2 = !digitalRead(2);
-  }
-  else if (!switchPress_4 && !switchPress_3 && !switchPress_2 && switchPress_1)
-  {
-    switchPress_3 = digitalRead(3);
-  }
-  else if (!switchPress_4 && switchPress_3 && !switchPress_2 && switchPress_1)
-  {
-    switchPress_4 = digitalRead(4);
-    if (switchPress_4)
-    {
-      switchPress_1 = false;
-    }
-  }
-  else if (switchPress_4 && switchPress_3 && !switchPress_2 && !switchPress_1)
-  {
-    switchPress_3 = !digitalRead(3);
-  }
-
-  // Error readouts
-  else if (switchPress_4 && switchPress_1)
-  {
-    Serial.print("Error: impossible direction");
-  }
-  else if (!switchPress_4 && !switchPress_1)
-  {
-    Serial.print("Error: no direction");
-  }
-  else if (switchPress_2 && switchPress_3)
-  {
-    Serial.print("Error: impossible location");
-  }
-}
-
-// Uses the center tap of the potentiometer to calibrate the joystick
-int calibrate (int backup)
-{
-  int middleVal = analogRead(CENTER_TAP);
-  int maxVal = middleVal * 2;
-
-  // back up in case that something with the center tap pin goes wrong
-  if (abs(maxVal - backup) >= 100 || counter > 500)
-  {
-    counter = 0;
-    return maxVal;
-  }
-  else
-  {
-    counter++; 
-    return backup;
-  }
-}
-
-
-// Adjust the values to account for the deadzone
-int deadzone(int joyVal)
-{
-  // Find the values that the deadzone cuts off
-  float high_deadzone_val = (joystickMax / 2) + JOYSTICK_DEADZONE;
-  float low_deadzone_val = (joystickMax / 2) - JOYSTICK_DEADZONE;
-
-  // Find the coefficient to adjust the values with
-  float high_deadzone_coef = (joystickMax - (joystickMax / 2)) / (joystickMax - high_deadzone_val);
-  float low_deadzone_coef = (JOYSTICK_MIN - (joystickMax / 2)) / (JOYSTICK_MIN - low_deadzone_val);
-
-// Scales the value from the deadzone to max
-  if (joyVal >= high_deadzone_val)
-  {
-    joyVal = high_deadzone_coef * (joyVal - high_deadzone_val) + (joystickMax / 2);
-  }
-  else if (joyVal <= low_deadzone_val)
-  {
-    joyVal = low_deadzone_coef * (joyVal - low_deadzone_val) + (joystickMax / 2);
-  }
-  else
-  {
-    joyVal = joystickMax / 2;
-  }
-  return joyVal;
-}
-
-// Prints the CVS of the time and the servo angle
-void printTable(int time)
-{
-    Serial.print(time / 1000);
-    Serial.print(",");
-    Serial.print(dxl.getPresentPosition(DXL_ID, UNIT_DEGREE) - 182);
-    Serial.println();
-}
-
-
-// increments the desired angle and decreses back to 0 when having reached 30 degrees
-void runThrough(bool forwards)
-{
-  if (abs(dxl.getPresentPosition(DXL_ID, UNIT_DEGREE) - 182) >= 29)
-  {
-    up = false;
-  }
-
-  if (forwards)
-  {
-    up ? angle++ : angle--;
-  }
-  else
-  {
-    up ? angle-- : angle++;
-  }
-}
-
-// Based on set intervals, calls the function to set the angle, increase the angle, or record the angle
-void gatherData(bool forward, int time)
-{
-  if (start && (time % 1000) < 800 && !pass) // if within the 20 millisecond interval 
-  {
-    printTable(time);    
-    pass = true;
-  }
-  else if(start && time % 2000 > 1800 && pass) // every 2 seconds
-  {
-    pass = false;
-    runThrough(forward);
-    dxl.setGoalPosition(DXL_ID, angle, UNIT_DEGREE);
-  }
-  else if(start && time % 1000 > 800 && pass) // ensures that a second is not counted twice
-  {
-    pass = false;
-  }
-}
-
-void testEndurance(bool forwards, int time)
-{
-  if (forwards)
-  {
-    if((time % 500) < 20 && !pass)
-    {
-      dxl.setGoalPosition(DXL_ID, rand() % 30 + 182, UNIT_DEGREE);
-    }
-    else if(time % 500 > 20 && pass)
-    {
-      pass = false;
-    }
-  }
-  else
-  {
-    if((time % 500) < 20 && !pass)
-    {
-      dxl.setGoalPosition(DXL_ID, 182 - (rand() % 30), UNIT_DEGREE);
-    }
-    else if(time % 500 > 20 && pass)
-    {
-      pass = false;
-    }
-  }
-}
-
-
+bool up = true;
 void loop() {
   int joystickValue = analogRead(JOYSTICK_PIN);
 
  
-  //joystickMax = calibrate(joystickMax);
+  //joystick.calibrate();
   //Serial.println(joystickMax);
-
-  // Apply deadzone to joystick value
-  if (abs(joystickValue - (joystickMax / 2)) < JOYSTICK_DEADZONE) {
-    joystickValue = joystickMax / 2;
-  }
   
   // Adjust the values to account for the deadzone
-  joystickValue = deadzone(joystickValue);
+  joystickValue = joystick.setDeadzone(joystickValue);
   
-  //readCyclePosition();
-  //cycleMotion();
+  //servo.readCyclePosition();
+  //servo.cycleMotion();
 
 // Map joystick value to servo angle
-  float servoAngle = map(joystickValue, JOYSTICK_MIN, joystickMax, SERVO_MIN_ANGLE, SERVO_MAX_ANGLE);
+  float servoAngle = map(joystickValue, JOYSTICK_MIN, joystick.getJoystickMax(), servo.getMinServoAngle(), servo.getMaxServoAngle());
 
   // the starting for the testing functions
-  
-  if (!start && digitalRead(5))
+  servo.init(dxl);
+  int currentTime = millis() - joystick.getStartTime();
+  if (abs(servoAngle - 180) < 2 && !joystick.getOverride())
   {
-    startTime = millis();
-    start = true;
-    angle = 182;
-    up = true;
+    //servo.gatherData(FORWARDS, currentTime, dxl);
+    //servo.testEndurance(FORWARDS, currentTime, dxl);
+    servo.readCyclePosition();
+    servo.cycleMotion(dxl, LOOP_SLOW_VELOCITY);
+    //Serial.print("test");
+    //servo.autoLoop(false, dxl);    
+    /*
+    if (up)
+    {
+      dxl.setGoalPosition(DXL_ID, SERVO_MAX_ANGLE, UNIT_DEGREE);
+      if(abs(dxl.getPresentPosition(DXL_ID, UNIT_DEGREE) - SERVO_MAX_ANGLE) < 1)
+      {
+        up = false;
+      }
+    }
+    else
+    {
+      dxl.setGoalPosition(DXL_ID, SERVO_MIN_ANGLE, UNIT_DEGREE);
+      if (abs(dxl.getPresentPosition(DXL_ID, UNIT_DEGREE) - SERVO_MIN_ANGLE) < 1)
+      {
+        up = true;
+      }
+
+    }
+
+    if (digitalRead(0))
+    {
+      up = true;
+    }
+    else if (digitalRead(4))
+    {
+      up = false;
+    }
+    */
+    
   }
-  else if (!up && abs(dxl.getPresentPosition(DXL_ID, UNIT_DEGREE)-182) < 0.25)
+  else
   {
-    start = false;
-  }
-  int currentTime = millis() - startTime;
-  //if (abs(servoAngle - 180) < 2 && !overRide)
-  //{
-    //gatherData(FORWARDS, currentTime);
-    //testEndurance(FORWARDS, currentTime);
- // }
-  //else
-  //{
 // Set servo goal position
- // dxl.setGoalPosition(DXL_ID, servoAngle, UNIT_DEGREE);
- // overRide = true;
- // }
+  dxl.setGoalPosition(DXL_ID, servoAngle, UNIT_DEGREE);
+  joystick.setOverride(true);
+  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, SERVO_MAX_VELOCITY);
+  Serial.println(digitalRead(5));
+    if(digitalRead(5))
+    {
+      joystick.setOverride(false);
+    }
+
+  }
+  //Serial.println(dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, 30));
 
    // Print the raw joystick value
-  Serial.print("Joystick Value: ");
-  Serial.println(joystickValue);
-
+  //Serial.print("Joystick Value: ");
+  //Serial.println(joystickValue);
+/*
+  Serial.print("switch 1: ");
+  Serial.println(digitalRead(0));
+  Serial.print("switch 2: ");
+  Serial.println(digitalRead(2));
+  Serial.print("switch 3: ");
+  Serial.println(digitalRead(3));
+  Serial.print("switch 4: ");
+  Serial.println(digitalRead(4));
+  */
   // Read and print the current servo position
+  
   float presentPosition = dxl.getPresentPosition(DXL_ID, UNIT_DEGREE);
   Serial.print("Present Position (degree): ");
-  Serial.println(presentPosition-180);
+  Serial.println(presentPosition);
   
   delay(10);
 }
